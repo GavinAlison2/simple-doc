867a63ee7da784074cf424a91868bc09acbe2776
alice <921757697@qq.com>
2025-03-31 15:40:43 +0800

    deploy

---
+++ assets/test4.html-BadI1rT2.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,b as a,o as r}from"./app-z9xtMaXe.js";const o={};function l(n,t){return r(),s("div",null,t[0]||(t[0]=[a("p",null,"test4",-1)]))}const p=e(o,[["render",l]]),d=JSON.parse('{"path":"/pages/folder2/test4.html","title":"","lang":"zh-CN","frontmatter":{},"headers":[],"git":{},"filePathRelative":"pages/folder2/test4.md"}');export{p as comp,d as data};
