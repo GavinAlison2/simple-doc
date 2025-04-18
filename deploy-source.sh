237c830c24e576c0a2380af5eca3ad998db2859f
alice <921757697@qq.com>
2025-04-18 08:06:30 +0800

    doc 整理

--- deploy-source.sh
+++ deploy-source.sh
@@ -24,7 +24,7 @@ git rm -r --cached docs/.vuepress/.temp
 git commit -m 'doc 整理'
 
 git pull origin master --allow-unrelated-histories
-git remote add origin git@github.com:GavinAlison2/simple-doc.git
+# git remote add origin git@github.com:GavinAlison2/simple-doc.git
 git branch -M master
 # git push  -f origin master:gh-pages
 git push -f origin master
